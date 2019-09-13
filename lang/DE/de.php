226e795e4a588eb56e98a83ec808bc52836875c1
Kristijan <kristijan80@web.de>
2019-09-13 22:50:18 +0200

    mentoring

--- lang/DE/de.php
+++ lang/DE/de.php
@@ -195,6 +195,8 @@ $g_lang['team_status_1'] = '<strong>Es werden noch Helfer gesucht</strong>';
 $g_lang['team_status_2'] = '<strong>Es werden dringend noch Helfer gesucht</strong>';
 $g_lang['attach_image'] = 'Bild anhängen';
 $g_lang['write_teaser'] = 'Schreibe etwas...';
+$g_lang['new_welcome'] = 'Neue Willkommen ! ';
+
 $g_lang['pubbtime_1'] = 'morgens';
 $g_lang['pubbtime_2'] = 'mittags/nachmittags';
 $g_lang['pubbtime_3'] = 'abends';
