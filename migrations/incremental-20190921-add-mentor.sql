51ef6d081780055d3ac282d0bf4ef15706db958c
Kristijan <kristijan80@web.de>
2019-09-22 21:57:59 +0200

    renaming mentor to tutor

--- migrations/incremental-20190921-add-mentor.sql
+++ migrations/incremental-20190921-add-mentor.sql
@@ -1,5 +1,5 @@
 alter table fs_betrieb
-    add allow_mentor tinyint(3) default 0 null after presse;
+    add allow_tutoring tinyint(3) default 0 null after presse;
 
 alter table fs_betrieb_team
-    add mentor tinyint(3) default 0 null after verantwortlich;
+    add tutor tinyint(3) default 0 null after verantwortlich;
