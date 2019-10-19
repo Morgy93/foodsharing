4d2ed684f649fa95be399cf9e20e657c04643aa3
Jano <jano.paetzold@gmail.com>
2019-10-19 20:21:24 +0200

    Refactor NotificationService

--- src/Services/NotificationService.php
+++ src/Services/NotificationService.php
@@ -31,46 +31,63 @@ final class NotificationService
 
 	public function newFoodSharePointPost(int $foodSharePointId)
 	{
-		if ($foodSharePoint = $this->foodSharePointGateway->getFoodSharePoint($foodSharePointId)) {
-			$post = $this->foodSharePointGateway->getLastFoodSharePointPost($foodSharePointId);
-			if ($followers = $this->foodSharePointGateway->getEmailFollower($foodSharePointId)) {
-				$body = nl2br($post['body']);
-
-				if (!empty($post['attach'])) {
-					$attach = json_decode($post['attach'], true);
-					if (isset($attach['image']) && !empty($attach['image'])) {
-						foreach ($attach['image'] as $img) {
-							$body .= '
-							<div>
-								<img src="' . BASE_URL . '/images/wallpost/medium_' . $img['file'] . '" />
-							</div>';
-						}
-					}
-				}
-
-				foreach ($followers as $f) {
-					$this->emailHelper->tplMail('foodSharePoint/new_message', $f['email'], array(
-						'link' => BASE_URL . '/?page=fairteiler&sub=ft&id=' . (int)$foodSharePointId,
-						'name' => $f['name'],
-						'anrede' => $this->translationHelper->genderWord($f['geschlecht'], 'Lieber', 'Liebe', 'Liebe/r'),
-						'fairteiler' => $foodSharePoint['name'],
-						'post' => $body
-					));
-				}
-			}
-
-			if ($followers = $this->foodSharePointGateway->getInfoFollowerIds($foodSharePointId)) {
-				$followersWithoutPostAuthor = array_diff($followers, [$post['fs_id']]);
-				$this->bellGateway->addBell(
-					$followersWithoutPostAuthor,
-					'ft_update_title',
-					'ft_update',
-					'img img-recycle yellow',
-					array('href' => '/?page=fairteiler&sub=ft&id=' . $foodSharePointId),
-					array('name' => $foodSharePoint['name'], 'user' => $post['fs_name'], 'teaser' => $this->sanitizerService->tt($post['body'], 100)),
-					'fairteiler-' . $foodSharePointId
-				);
-			}
+		$foodSharePoint = $this->foodSharePointGateway->getFoodSharePoint($foodSharePointId);
+		if (!$foodSharePoint) {
+			return;
 		}
+		$post = $this->foodSharePointGateway->getLastFoodSharePointPost($foodSharePointId);
+
+		$this->sendEmailNotification($foodSharePoint, $post);
+		$this->sendBellNotification($foodSharePoint, $post);
+	}
+
+	private function sendEmailNotification(array $foodSharePoint, array $post): void
+	{
+		$eMailFollowers = $this->foodSharePointGateway->getEmailFollower($foodSharePoint['id']);
+		if (!$eMailFollowers || empty($post['attach'])) {
+			return;
+		}
+
+		$attach = json_decode($post['attach'], true);
+		if (!isset($attach['image']) && empty($attach['image'])) {
+			return;
+		}
+
+		$body = nl2br($post['body']);
+		foreach ($attach['image'] as $img) {
+			$body .= '
+			<div>
+				<img src="' . BASE_URL . '/images/wallpost/medium_' . $img['file'] . '" />
+			</div>';
+		}
+
+		foreach ($eMailFollowers as $f) {
+			$this->emailHelper->tplMail('foodSharePoint/new_message', $f['email'], array(
+				'link' => BASE_URL . '/?page=fairteiler&sub=ft&id=' . (int)$foodSharePoint['id'],
+				'name' => $f['name'],
+				'anrede' => $this->translationHelper->genderWord($f['geschlecht'], 'Lieber', 'Liebe', 'Liebe/r'),
+				'fairteiler' => $foodSharePoint['name'],
+				'post' => $body
+			));
+		}
+	}
+
+	private function sendBellNotification(array $foodSharePoint, array $post): void
+	{
+		$infoFollowers = $this->foodSharePointGateway->getInfoFollowerIds($foodSharePoint['id']);
+		if (!$infoFollowers) {
+			return;
+		}
+
+		$followersWithoutPostAuthor = array_diff($infoFollowers, [$post['fs_id']]);
+		$this->bellGateway->addBell(
+			$followersWithoutPostAuthor,
+			'ft_update_title',
+			'ft_update',
+			'img img-recycle yellow',
+			array('href' => '/?page=fairteiler&sub=ft&id=' . $foodSharePoint['id']),
+			array('name' => $foodSharePoint['name'], 'user' => $post['fs_name'], 'teaser' => $this->sanitizerService->tt($post['body'], 100)),
+			'fairteiler-' . $foodSharePoint['id']
+		);
 	}
 }
