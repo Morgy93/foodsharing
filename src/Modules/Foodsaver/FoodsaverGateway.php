36cbd125307a5192a65eebf2c8e0f84461e15c10
Kristijan <kristijan80@web.de>
2020-02-14 17:41:21 +0100

    home district (bezirk_id) added to fs_change_history.  leaving home district removes verification and tracked in fs_verify_history

--- src/Modules/Foodsaver/FoodsaverGateway.php
+++ src/Modules/Foodsaver/FoodsaverGateway.php
@@ -769,10 +769,31 @@ final class FoodsaverGateway extends BaseGateway
 		$mainRegion_id = $this->db->fetchValueByCriteria('fs_foodsaver', 'bezirk_id', ['id' => $fsId]);
 		if ($mainRegion_id === $regionId) {
 			$this->db->update('fs_foodsaver', [
-				'bezirk_id' => 0
+				'bezirk_id' => 0,
+				'verified'  => 0
 			], [
 				'id' => $fsId
 			]);
+			$this->db->insert(
+				'fs_foodsaver_change_history',
+				[
+					'date' => date(\DateTime::ISO8601),
+					'fs_id' => $fsId,
+					'changer_id' => $fsId,
+					'object_name' => 'bezirk_id',
+					'old_value' => $mainRegion_id,
+					'new_value' => 0
+				]
+			);
+			$this->db->insert(
+				'fs_verify_history',
+				[
+					'fs_id' => $fsId,
+					'date' => date(\DateTime::ISO8601),
+					'bot_id' => $fsId,
+					'change_status' => 0
+				]
+			);
 		}
 	}
 
