321f1d7e7d7c03b44c10e55a9427e77381913db4
Rafael Muselmann <rafael.muselmann@gmx.de>
2020-03-29 14:50:09 +0200

    Updates phone example to lang file.

--- src/Modules/Register/components/RegisterMobilephone.vue
+++ src/Modules/Register/components/RegisterMobilephone.vue
@@ -37,6 +37,7 @@
 </template>
 <script>
 import { VueTelInput } from 'vue-tel-input'
+import i18n from '@/i18n'
 
 export default {
   components: {
@@ -50,7 +51,7 @@ export default {
         mode: 'international',
         defaultCountry: 'DE',
         disabledFetchingCountry: true,
-        placeholder: 'Beispiel: 17912345678',
+        placeholder: i18n('register.phone_example'),
         preferredCountries: ['DE', 'AT', 'CH'],
         name: 'mobilephone',
         maxLen: 18,
