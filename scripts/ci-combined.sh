cc7937e6c67c0f105391dc584d44a62ecee5327f
Matthias Larisch <mail@matthias-larisch.de>
2020-05-20 09:21:31 +0200

    Use newer image, disable xdebug

--- scripts/ci-combined.sh
+++ scripts/ci-combined.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+set -x
+
 cp docker/conf/ci_combined/supervisor_apps.conf /etc/supervisor/conf.d/
+rm /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 cp .codeception.ci.env .codeception.env
 sed -i "s#/app#$CI_PROJECT_DIR#g" /etc/supervisor/conf.d/supervisor_apps.conf
 sed -i "s#/app#$CI_PROJECT_DIR#g" /usr/local/etc/php-fpm.d/fpm.conf
@@ -7,7 +11,6 @@ mysql -u root -proot -hdb --execute="drop database if exists foodsharing; create
 cat migrations/initial.sql migrations/static.sql migrations/27-profilchange.sql migrations/27-verify.sql migrations/incremental-* | mysql -uroot -proot -hdb foodsharing
 chown -R www-data:www-data .
 chown -R www-data:www-data /var/www
-apt-get update && apt-get install sudo
 /start.sh &
 sudo -u www-data composer install --verbose --prefer-dist --no-progress --no-interaction --no-suggest --no-scripts --ignore-platform-reqs
 sudo -u www-data vendor/bin/codecept run acceptance ForumPostCest
