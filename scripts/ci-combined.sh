515ef64eba52e3a0d8071298389c01681d880e2e
Matthias Larisch <mail@matthias-larisch.de>
2020-05-21 09:57:15 +0200

    Do test execution as in ci.test

--- scripts/ci-combined.sh
+++ scripts/ci-combined.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+
+export FS_ENV=ci
+
+set -o errexit
 set -x
+
+dir=$(dirname "$0")
+
+# shellcheck source=./inc.sh
+source "$dir"/inc.sh
+echo $SECONDS seconds elapsed
+
 
 cp docker/conf/ci_combined/supervisor_apps.conf /etc/supervisor/conf.d/
 rm /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
@@ -15,5 +26,53 @@ chown -R www-data:www-data .
 chown -R www-data:www-data /var/www
 /start.sh &
 sudo -u www-data composer install --verbose --prefer-dist --no-progress --no-interaction --no-suggest --no-scripts --ignore-platform-reqs
-sudo -u www-data vendor/bin/codecept run acceptance ForumPostCest
-sudo -u www-data vendor/bin/codecept run functional
+
+log-header "Running tests"
+failed=0
+SECONDS=0
+sudo -u www-data vendor/bin/codecept run --xml --html || failed=1
+echo $SECONDS seconds elapsed
+
+if [ $failed -eq 1 ]; then
+  log-header "Check for codeception errors"
+  # check if codeception generated a report file that contains failed tests
+  # Otherwise, codeception probably failed itself and the whole job should fail
+  grep -E '<error|<failure' /app/tests/_output/report.xml || (echo "report.xml is incomplete, aborting" && false)
+
+  log-header "Rerunning failed tests"
+  SECONDS=0
+  sudo -u www-data vendor/bin/codecept run --xml --html -g failed || failed=2
+  echo $SECONDS seconds elapsed
+fi
+
+if [ $failed -eq 2 ]; then
+  log-header "Check for codeception errors"
+  # check if codeception generated a report file that contains failed tests
+  # Otherwise, codeception probably failed itself and the whole job should fail
+  grep -E '<error|<failure' /app/tests/_output/report.xml || (echo "report.xml is incomplete, aborting" && false)
+
+  log-header "Rerunning failed tests"
+  SECONDS=0
+  sudo -u www-data vendor/bin/codecept run --xml --html -g failed || failed=3
+  echo $SECONDS seconds elapsed
+fi
+
+if [ $failed -eq 3 ]; then
+  log-header "Check for codeception errors"
+  # check if codeception generated a report file that contains failed tests
+  # Otherwise, codeception probably failed itself and the whole job should fail
+  grep -E '<error|<failure' /app/tests/_output/report.xml || (echo "report.xml is incomplete, aborting" && false)
+
+  log-header "Rerunning failed tests"
+  SECONDS=0
+  sudo -u www-data vendor/bin/codecept run -g failed
+  echo $SECONDS seconds elapsed
+fi
+
+log-header "Running chat tests"
+SECONDS=0
+cd chat
+yarn lint && yarn test
+echo $SECONDS seconds elapsed
+
+log-header "Done!"
