d8ffcfb9b7d36915b8b896433bcff786eb7e507f
Chris Oelmueller <chris.oelmueller@gmail.com>
2020-05-30 16:42:36 +0200

    StoreEdit: address MR comment (no ready() needed)

--- src/Modules/Store/Store.js
+++ src/Modules/Store/Store.js
@@ -1,6 +1,5 @@
 import '@/core'
 import '@/globals'
-import $ from 'jquery'
 import 'jquery-dynatree'
 import { vueRegister, vueApply } from '@/vue'
 import StoreEdit from './components/StoreEdit.vue'
@@ -10,30 +9,23 @@ import {
   GET
 } from '@/script'
 
-$(document).ready(() => {
-  switch (GET('a')) {
-    case 'edit': {
-      vueRegister({
-        StoreEdit
-      })
-      vueApply('#vue-storeedit')
-      attachAddressPicker()
-      break
-    }
-    case 'new': {
-      // ...
-      attachAddressPicker()
-      break
-    }
-    case undefined: {
-      vueRegister({
-        StoreList
-      })
-      vueApply('#vue-storelist', true)
-      break
-    }
-    default: {
-      break
-    }
+switch (GET('a')) {
+  case 'edit': {
+    vueRegister({ StoreEdit })
+    vueApply('#vue-storeedit')
+    attachAddressPicker()
+    break
   }
-})
+  case 'new': {
+    attachAddressPicker()
+    break
+  }
+  case undefined: {
+    vueRegister({ StoreList })
+    vueApply('#vue-storelist', true)
+    break
+  }
+  default: {
+    break
+  }
+}
