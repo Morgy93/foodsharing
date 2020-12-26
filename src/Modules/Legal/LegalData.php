908f485294abc60d94f6d915c66537e39a830850
Chris Oelmueller <chris.oelmueller@gmail.com>
2020-12-26 13:10:08 +0100

    Remove unused LegalData boilerplate
    this entire form is overengineered...

--- src/Modules/Legal/LegalData.php
+++ src/Modules/Legal/LegalData.php
@@ -7,15 +7,10 @@ use Symfony\Component\Validator\Constraints as Assert;
 class LegalData
 {
 	/**
-	 * @Assert\Type("boolean")
 	 * @Assert\IsTrue(message="legal.must_accept_pp")
 	 */
-	private $privacyPolicyAcknowledged;
-
-	/**
-	 * @Assert\Type("boolean")
-	 */
-	private $privacyNoticeAcknowledged;
+	private bool $privacyPolicyAcknowledged;
+	private bool $privacyNoticeAcknowledged;
 
 	public function __construct(bool $privacyPolicyAcknowledged = false, bool $privacyNoticeAcknowledged = false)
 	{
@@ -23,27 +18,8 @@ class LegalData
 		$this->privacyNoticeAcknowledged = $privacyNoticeAcknowledged;
 	}
 
-	public function isPrivacyPolicyAcknowledged(): bool
-	{
-		return $this->privacyPolicyAcknowledged;
-	}
-
-	public function setPrivacyPolicyAcknowledged(?bool $acknowledged): void
-	{
-		if ($acknowledged !== null) {
-			$this->privacyPolicyAcknowledged = $acknowledged;
-		}
-	}
-
 	public function isPrivacyNoticeAcknowledged(): bool
 	{
 		return $this->privacyNoticeAcknowledged;
-	}
-
-	public function setPrivacyNoticeAcknowledged(?bool $acknowledged): void
-	{
-		if ($acknowledged !== null) {
-			$this->privacyNoticeAcknowledged = $acknowledged;
-		}
 	}
 }
