ade375dac9cc47a3b422d8a913ed6c85e8617f5c
Christian Walgenbach <chris@walgeo.de>
2021-12-15 23:06:34 +0100

    Added a vue js modal before start the quiz

--- src/Modules/Settings/components/QuizIndex.vue
+++ src/Modules/Settings/components/QuizIndex.vue
@@ -42,10 +42,9 @@
               class="mb-4"
             >
               <b-button
+                v-b-modal.modal-1
                 variant="secondary"
                 class="pl-5 pr-5"
-                :href="$url('quizFoodsaverWiki')"
-                target="_blank"
               >
                 {{ $i18n('settings.foodsaver_quiz.quiz_time_limit') }}
               </b-button>
@@ -53,13 +52,49 @@
             <b-col>
               <div>
                 <b-button
+                  v-b-modal.modal-1
                   variant="secondary"
                   class="pl-5 pr-5"
-                  :href="$url('quizFoodsaverWiki')"
-                  target="_blank"
                 >
                   {{ $i18n('settings.foodsaver_quiz.quiz_without_time_limit') }}
                 </b-button>
+                <b-modal
+                  id="modal-1"
+                  modal-class="bootstrap"
+                  header-class="d-flex"
+                  content-class="pr-3 pt-3"
+                  :cancel-title="$i18n('button.cancel')"
+                  :ok-title="$i18n('settings.quiz_modal.button_start')"
+                  :title="quizName + ': '+ $i18n('settings.quiz_modal.title')"
+                  @ok="startQuiz"
+                >
+                  <p>
+                    <i class="fas fa-glasses" /> {{ $i18n('settings.quiz_modal.before_title') }}
+                  </p>
+                  <ul class="mb-4">
+                    <li>{{ $i18n('settings.quiz_modal.before_text') }}</li>
+                  </ul>
+                  <p>
+                    <i class="fas fa-question" /> {{ $i18n('settings.quiz_modal.during_title') }}
+                  </p>
+                  <ul class="mb-4">
+                    <li>{{ $i18n('settings.quiz_modal.during_text_1') }}</li>
+                    <li>{{ $i18n('settings.quiz_modal.during_text_2') }}</li>
+                    <li>{{ $i18n('settings.quiz_modal.during_text_3') }}</li>
+                    <li>{{ $i18n('settings.quiz_modal.during_text_4') }}</li>
+                  </ul>
+                  <p>
+                    <i class="fas fa-check-circle" /> {{ $i18n('settings.quiz_modal.after_title') }}
+                  </p>
+                  <ul>
+                    <li>{{ $i18n('settings.quiz_modal.after_text_1') }}</li>
+                    <li>{{ $i18n('settings.quiz_modal.after_text_2') }}</li>
+                  </ul>
+                  <p>
+                    {{ $i18n('settings.quiz_modal.hint_title') }}:<br>
+                    {{ $i18n('settings.quiz_modal.hint_text') }}
+                  </p>
+                </b-modal>
               </div>
             </b-col>
           </b-row>
@@ -71,11 +106,17 @@
 </template>
 
 <script>
+import { ajreq } from '@/script'
 
 export default {
   props: {
     quizId: { type: Number, default: null },
     quizName: { type: String, default: '' },
+  },
+  methods: {
+    startQuiz () {
+      ajreq('startquiz', { app: 'quiz', qid: this.quizId })
+    },
   },
 }
 </script>
