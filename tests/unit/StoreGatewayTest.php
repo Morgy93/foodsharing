6a6c798074504689113e5add653b95cb7e1e56c4
KoeMai <10832025-KoeMai@users.noreply.gitlab.com>
2022-06-26 12:41:04 +0200

    Fix test file style

--- tests/unit/StoreGatewayTest.php
+++ tests/unit/StoreGatewayTest.php
@@ -2,9 +2,9 @@
 
 use Faker\Factory;
 use Faker\Generator;
+use Foodsharing\Modules\Core\DBConstants\Store\CooperationStatus;
 use Foodsharing\Modules\Store\StoreGateway;
 use Foodsharing\Modules\Store\TeamStatus;
-use Foodsharing\Modules\Core\DBConstants\Store\CooperationStatus;
 
 class StoreGatewayTest extends \Codeception\Test\Unit
 {
@@ -124,39 +124,47 @@ class StoreGatewayTest extends \Codeception\Test\Unit
 		$this->tester->assertEquals(0, $conversationId);
 	}
 
-
-
-
 	public function testFoodsaverRelatedStoreMembershipStatus()
 	{
-		$store1 = $this->tester->createStore($this->region['id'], null, null, ["betrieb_status_id"=>CooperationStatus::COOPERATION_ESTABLISHED]);
-		$store2 = $this->tester->createStore($this->region['id'], null, null, ["betrieb_status_id"=>CooperationStatus::COOPERATION_ESTABLISHED]);
-		$store3 = $this->tester->createStore($this->region['id'], null, null, ["betrieb_status_id"=>CooperationStatus::COOPERATION_ESTABLISHED]);
-		$store4 = $this->tester->createStore($this->region['id'], null, null, ["betrieb_status_id"=>CooperationStatus::COOPERATION_ESTABLISHED]);
+		$store1 = $this->tester->createStore($this->region['id'], null, null, ['betrieb_status_id' => CooperationStatus::COOPERATION_ESTABLISHED]);
+		$store2 = $this->tester->createStore($this->region['id'], null, null, ['betrieb_status_id' => CooperationStatus::COOPERATION_ESTABLISHED]);
+		$store3 = $this->tester->createStore($this->region['id'], null, null, ['betrieb_status_id' => CooperationStatus::COOPERATION_ESTABLISHED]);
+		$store4 = $this->tester->createStore($this->region['id'], null, null, ['betrieb_status_id' => CooperationStatus::COOPERATION_ESTABLISHED]);
 
 		$this->tester->addStoreTeam($store1['id'], $this->foodsaver['id'], true, false, true); // Test coordinator
 		$this->tester->addStoreTeam($store2['id'], $this->foodsaver['id'], false, true, true); // Test waiting for membership (JUMPER)
 		$this->tester->addStoreTeam($store3['id'], $this->foodsaver['id'], false, false, true); // Test membership (MEMBER)
 		$this->tester->addStoreTeam($store4['id'], $this->foodsaver['id'], false, false, false); // Test open request confirmed (Pending request)
-		$this->tester->addStoreTeam($store2['id'], $this->foodsaver['id']+1, false, false, false); // Test open request confirmed (Pending request)
+		$this->tester->addStoreTeam($store2['id'], $this->foodsaver['id'] + 1, false, false, false); // Test open request confirmed (Pending request)
 
-		$expectation = [["betrieb_id"=>$store1['id'], "name" => $store1['name'], "managing"=> 1, "membershipstatus"=> 1],
-			["betrieb_id"=>$store2['id'], "name" => $store2['name'], "managing"=> 0, "membershipstatus"=> 2],
-			["betrieb_id"=>$store3['id'], "name" => $store3['name'], "managing"=> 0, "membershipstatus"=> 1],
-			["betrieb_id"=>$store4['id'], "name" => $store4['name'], "managing"=> 0, "membershipstatus"=> 0]
+		$expectation = [['betrieb_id' => $store1['id'], 'name' => $store1['name'], 'managing' => 1, 'membershipstatus' => 1],
+						['betrieb_id' => $store2['id'], 'name' => $store2['name'], 'managing' => 0, 'membershipstatus' => 2],
+						['betrieb_id' => $store3['id'], 'name' => $store3['name'], 'managing' => 0, 'membershipstatus' => 1],
+						['betrieb_id' => $store4['id'], 'name' => $store4['name'], 'managing' => 0, 'membershipstatus' => 0]
 		];
-		usort($expectation, function ($a , $b) {
-			if ($a["managing"]==$b["managing"]) {
-				if ($a["membershipstatus"]==$b["membershipstatus"]) {
-					if ($a["name"]==$b["name"]) return 0;
-					if ($a["name"] < $b["name"]) return -1;
-					else return 1;
+		usort($expectation, function ($a, $b) {
+			if ($a['managing'] == $b['managing']) {
+				if ($a['membershipstatus'] == $b['membershipstatus']) {
+					if ($a['name'] == $b['name']) {
+						return 0;
+					}
+					if ($a['name'] < $b['name']) {
+						return -1;
+					} else {
+						return 1;
+					}
 				}
-				if ($a["membershipstatus"] < $b["membershipstatus"]) return -1;
-				else return 1;
-			} 
-			if ($a["managing"] > $b["managing"]) return -1;
-			else return 1;
+				if ($a['membershipstatus'] < $b['membershipstatus']) {
+					return -1;
+				} else {
+					return 1;
+				}
+			}
+			if ($a['managing'] > $b['managing']) {
+				return -1;
+			} else {
+				return 1;
+			}
 		});
 		$result = $this->gateway->listAllStoreTeamMembershipsForFoodsaver($this->foodsaver['id']);
 		$this->assertEquals($expectation, $result);
