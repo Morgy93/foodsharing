03cc7a5703837a98f4f6bb7158646c346ca1bdb4
Stefan <you@example.com>
2022-06-27 21:47:08 +0200

    styleguide improvments

--- client/src/views/partials/Styleguide/StyleguideModal.vue
+++ client/src/views/partials/Styleguide/StyleguideModal.vue
@@ -9,33 +9,40 @@
     <div class="modal-dialog modal-xl">
       <div class="modal-content">
         <div class="modal-header">
-          <h3>Styleguide</h3>
+          <h1>Styleguide</h1>
         </div>
         <div
           class="modal-body row"
         >
-          <div class="col-12 col-sm-6">
-            <h3>Colors</h3>
+          <div class="col-12 mb-5">
+            <h2>Colors</h2>
             <hr>
             <div class="flex">
               <div
-                v-for="(rule, idx) in rules"
+                v-for="(color, idx) in cssColorRules"
                 :key="idx"
-                data-show-as="tooltip"
-                data-placement="bottom"
-                :data-original-title="rule"
-                :aria-label="rule"
-                class="box copy"
-                :style="`background-color: var(${rule})`"
-                @click="copyToClipBoard(rule)"
-                v-html="rule"
-              />
+                class="col col-6"
+              >
+                <small
+                  v-html="color[0].type.name.toUpperCase()"
+                />
+                <div class="flex">
+                  <div
+                    v-for="({type, rule, base}) in color"
+                    :key="rule"
+                    class="box"
+                    :class="{ 'base': base }"
+                    :style="`background-color: var(${rule})`"
+                    :data-content="type.step"
+                    @click="copyToClipBoard(rule)"
+                  />
+                </div>
+              </div>
             </div>
           </div>
           <div class="col-12 col-sm-6">
             <div class="mb-5">
-              <h3>Icons</h3>
-              <a href="https://fontawesome.com/v5/search?m=free&s=solid%2Cbrands">Font Awesome V5 (FREE)</a>
+              <h2>Icons <a href="https://fontawesome.com/v5/search?m=free&s=solid%2Cbrands">(Font Awesome V5 [FREE])</a></h2>
               <hr>
               <div class="flex">
                 <i
@@ -48,29 +55,12 @@
               </div>
             </div>
             <div class="mb-5">
-              <h3>Fonts</h3>
-              <hr>
-              <div>
-                <h1>Headline #1, The quick brown fox jumps over the lazy dog</h1>
-                <h2>Headline #2, The quick brown fox jumps over the lazy dog</h2>
-                <h3>Headline #3, The quick brown fox jumps over the lazy dog</h3>
-                <h4>Headline #4, The quick brown fox jumps over the lazy dog</h4>
-                <h5>Headline #5, The quick brown fox jumps over the lazy dog</h5>
-                <h6>Headline #6, The quick brown fox jumps over the lazy dog</h6>
-                <hr>
-                <p>Body, The quick brown fox jumps over the lazy dog</p>
-                <p><a href="#">Link, The quick brown fox jumps over the lazy dog</a></p>
-                <p><b>B, The quick brown fox jumps over the lazy dog</b></p>
-                <p><strong>Strong, The quick brown fox jumps over the lazy dog</strong></p>
-                <p><i>Italic, The quick brown fox jumps over the lazy dog</i></p>
-                <p><em>EM, The quick brown fox jumps over the lazy dog</em></p>
-                <p><small>Small, The quick brown fox jumps over the lazy dog</small></p>
-                <p><code>Code, The quick brown fox jumps over the lazy dog</code></p>
-              </div>
-            </div>
-            <div class="mb-5">
-              <h3>Components</h3>
-              <a href="https://getbootstrap.com/docs/4.6/getting-started/introduction/">Bootstrap V4.6</a>
+              <h2>
+                <a
+                  href="https://getbootstrap.com/docs/4.6/getting-started/introduction/"
+                  v-html="'Bootstrap 4.6'"
+                />
+              </h2>
               <hr>
               <div class="flex">
                 <button
@@ -114,16 +104,21 @@
               </div>
             </div>
           </div>
-
-          <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-sm btn-light"
-              data-dismiss="modal"
-            >
-              You can use the <code class="key">ESC</code> hotkey or click here to close this modal.
-            </button>
+          <div class="col-12 col-sm-6">
+            <div class="mb-5">
+              <h2>Fonts</h2>
+              <hr>
+              <div v-html="generateFontRules()" />
+            </div>
           </div>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-sm btn-light"
+            data-dismiss="modal"
+            v-html="$i18n('globals.modal.close')"
+          />
         </div>
       </div>
     </div>
@@ -134,20 +129,91 @@
 export default {
   data () {
     return {
-      rules: [],
+      cssColorRules: [],
+      cssFontRules: [
+        'h1',
+        'h2',
+        'h3',
+        'h4',
+        'h5',
+        'h6',
+        'p',
+        'span',
+        'a',
+        'b',
+        'strong',
+        'i',
+        'em',
+        'small',
+        'code',
+        'mark',
+        'ins',
+        'del',
+        'sup',
+        'sub',
+        'blockquote',
+        'pre',
+        'ul',
+        'ol',
+        'dl',
+      ],
     }
   },
-  async created () {
-    this.rules = Array.from(document.styleSheets)
+  created () {
+    const cssColorRules = Array.from(document.styleSheets)
       .filter(sheet => sheet.href === null || sheet.href.startsWith(window.location.origin))
-      .reduce(
-        (acc, sheet) =>
-          (acc = [...acc, ...Array.from(sheet.cssRules)
-            .reduce((def, rule) =>
-              (def = rule.selectorText === ':root' ? [...def, ...Array.from(rule.style).filter(name => name.startsWith('--') && name.includes('color'))] : def), []),
-          ]), [])
+      .reduce((acc, sheet) => (acc = [...acc, ...Array.from(sheet.cssRules)]), [])
+      .filter(rule => rule.selectorText === ':root')
+      .reduce((acc, sheet) => (acc = [...acc, ...Array.from(sheet.style)]), [])
+      .filter(name => name.startsWith('--') && name.includes('color'))
+      .map((rule) => ({ type: this.extractColor(rule), rule }))
+    this.cssColorRules = this.chunkArray(cssColorRules, 9)
   },
   methods: {
+    generateFontRules () {
+      return this.cssFontRules
+        .map((font) => {
+          const str = []
+          str.push('<div>')
+          if (font === 'a') {
+            str.push(`<${font} href="#">`)
+          } else {
+            str.push(`<${font}>`)
+          }
+
+          if (['ul', 'ol'].includes(font)) {
+            str.push(`<li>${font} li, The quick brown fox jumps over ...</li>`)
+            str.push(`<li>${font} li, The quick brown fox jumps over ...</li>`)
+            str.push(`<li>${font} li, The quick brown fox jumps over ...</li>`)
+            str.push(`<li>${font} li, The quick brown fox jumps over ...</li>`)
+          } else if (font === 'dl') {
+            str.push(`<dt>${font} dt, The quick brown fox jumps over ...</dt>`)
+            str.push(`<dd>${font} dd, The quick brown fox jumps over ...</dd>`)
+            str.push(`<dt>${font} dt, The quick brown fox jumps over ...</dd>`)
+            str.push(`<dd>${font} dd, The quick brown fox jumps over ...</dd>`)
+          } else {
+            str.push(`${font}, The quick brown fox jumps over ...`)
+          }
+          str.push(`</${font}>`)
+          str.push('</div>')
+          return str.join('')
+        })
+        .join('')
+    },
+    extractColor (color) {
+      const regex = /--fs-color-(.+)-(\d+)/gm
+      const match = regex.exec(color) || ['', 'others', color]
+      console.log(match)
+      return match ? { name: match[1], step: match[2] } : 'OTHERS'
+    },
+    chunkArray (myArray, chunkSize) {
+      const arrayLength = myArray.length
+      const tempArray = []
+      for (let index = 0; index < arrayLength; index += chunkSize) {
+        tempArray.push(myArray.slice(index, index + chunkSize))
+      }
+      return tempArray
+    },
     copyToClipBoard (text) {
       navigator.clipboard.writeText(text).then(function () {
         console.log('Async: Copying to clipboard was successful!')
@@ -160,28 +226,42 @@ export default {
 </script>
 <style lang="scss" scoped>
 .box {
-  width: 100%;
-  height: 1rem;
+  width: 30%;
+  height: 1.25rem;
+  border-radius: var(--border-radius);
+  margin: 4px;
+  cursor: copy;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:after {
+    content: attr(data-content);
+    mix-blend-mode: difference;
+    color: white;
+    font-size: 0.7rem;
+  }
 
   &:hover {
-    outline: 2px solid goldenrod;
+    outline: 2px solid var(--fs-color-dark);
   }
-}
-
-.copy {
-  cursor: copy;
 }
 
 .btn {
   margin: .1rem;
 }
 
+.col:not(:last-child) {
+  padding-right: 0;
+}
+
 .flex {
   display: flex;
   flex-flow: wrap;
 
-  & > * {
-    flex: 1 1 auto;
+  &:not(:last-child) {
+    margin-bottom: 1rem;
+    margin-right: 1rem;
   }
 }
 </style>
