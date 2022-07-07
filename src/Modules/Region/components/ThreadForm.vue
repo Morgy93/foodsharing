164a05e45d47ecd3140321b0e2a8fbe5de09a84f
Stefan <you@example.com>
2022-07-07 10:36:23 +0200

    refactor(markdownModal): implemented at forum threads peace

--- src/Modules/Region/components/ThreadForm.vue
+++ src/Modules/Region/components/ThreadForm.vue
@@ -10,30 +10,41 @@
           ref="textarea"
           v-model="text"
           class="form-control"
+          :placeholder="$i18n('globals.placeholder.write_something')"
           rows="3"
           @keyup.ctrl.enter="submit"
         />
       </div>
       <div class="card-footer below">
-        <div class="row">
-          <div class="col">
-            <button
-              :disabled="!text.trim()"
-              class="btn btn-primary float-right"
-              @click="submit"
-            >
-              {{ $i18n('button.send') }}
-            </button>
-          </div>
-        </div>
+        <button
+          class="btn btn-sm btn-light"
+          @click="$bvModal.show('markdownPreviewModal')"
+        >
+          {{ $i18n('markdown.title') }}
+        </button>
+        <button
+          :disabled="!text.trim()"
+          class="btn btn-primary"
+          @click="submit"
+        >
+          {{ $i18n('button.send') }}
+        </button>
       </div>
     </div>
+    <markdownPreviewModal
+      :input.sync="text"
+      @input="updateText"
+    />
   </div>
 </template>
 
 <script>
+import markdownPreviewModal from '@/views/partials/Modals/MarkdownPreviewModal.vue'
 
 export default {
+  components: {
+    markdownPreviewModal,
+  },
   props: {},
   data () {
     return {
@@ -46,6 +57,9 @@ export default {
       this.$emit('submit', this.text.trim())
       this.text = ''
     },
+    updateText (text) {
+      this.text = text
+    },
     focus () {
       this.$refs.textarea.focus()
     },
@@ -54,5 +68,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.card-footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 </style>
