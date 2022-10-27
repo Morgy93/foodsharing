b75e3c021ada8bb43576bed2202658bb0ca94214
MS <s.mairbaeurl+gitlab@gmail.com>
2022-10-27 23:52:24 +0000

    max distance increased to see all of germany

--- src/Modules/Core/DBConstants/Map/MapConstants.php
+++ src/Modules/Core/DBConstants/Map/MapConstants.php
@@ -9,7 +9,7 @@ class MapConstants
 	public const CENTER_GERMANY = MapConstants::CENTER_GERMANY_LAT . ',' . MapConstants::CENTER_GERMANY_LON;
 	public const DEFAULT_SEARCH_DISTANCE = 45;
 	public const MIN_SEARCH_DISTANCE = 1;
-	public const MAX_SEARCH_DISTANCE = 350;
+	public const MAX_SEARCH_DISTANCE = 1000;
 	public const ZOOM_COUNTRY = 6;
 	public const ZOOM_CITY = 13;
 }
