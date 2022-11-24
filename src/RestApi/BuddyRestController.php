18fd96f8b1bc1b49816355f24c0d772de19586d2
BibaltiK <bibaltik@exdrals.de>
2022-11-24 20:48:07 +0100

    refactor RestApiController

--- src/RestApi/BuddyRestController.php
+++ src/RestApi/BuddyRestController.php
@@ -7,6 +7,9 @@ use Foodsharing\Modules\Buddy\BuddyGateway;
 use Foodsharing\Modules\Buddy\BuddyTransactions;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+
+use function in_array;
+
 use OpenApi\Annotations as OA;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
@@ -14,31 +17,22 @@ use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class BuddyRestController extends AbstractFOSRestController
 {
-	private BuddyTransactions $buddyTransactions;
-	private BuddyGateway $buddyGateway;
-	private Session $session;
-
 	public function __construct(
-		BuddyTransactions $buddyTransactions,
-		BuddyGateway $buddyGateway,
-		Session $session
+		private readonly BuddyTransactions $buddyTransactions,
+		private readonly BuddyGateway $buddyGateway,
+		private readonly Session $session
 	) {
-		$this->buddyTransactions = $buddyTransactions;
-		$this->buddyGateway = $buddyGateway;
-		$this->session = $session;
 	}
 
 	/**
 	 * Sends a buddy request to a user.
 	 *
-	 * @OA\Parameter(name="userId", in="path", @OA\Schema(type="integer"), description="which user to send the request to")
-	 * @OA\Response(response="200", description="Success.", @OA\Schema(type="object",
-	 *     @OA\Property(property="isBuddy", type="integer", description="whether the other user is now this user's buddy")
-	 * ))
-	 * @OA\Response(response="400", description="Already buddy with that user.")
-	 * @OA\Response(response="403", description="Insufficient permissions to send the request.")
 	 * @OA\Tag(name="buddy")
 	 * @Rest\Put("buddy/{userId}", requirements={"userId" = "\d+"})
+	 * @OA\Parameter(name="userId", in="path", @OA\Schema(type="integer"), description="which user to send the request to")
+	 * @OA\Response(response=Response::HTTP_OK, description="Success.")
+	 * @OA\Response(response=Response::HTTP_UNAUTHORIZED, description="Not logged in.")
+	 * @OA\Response(response=Response::HTTP_BAD_REQUEST, description="Already buddy with that user.")
 	 */
 	public function sendRequestAction(int $userId): Response
 	{
@@ -50,13 +44,13 @@ class BuddyRestController extends AbstractFOSRestController
 			throw new BadRequestHttpException();
 		}
 
-		$accepting = $this->buddyGateway->buddyRequestedMe($userId, $this->session->id());
-		if ($accepting) {
+		$isBuddyRequestForMe = $this->buddyGateway->buddyRequestedMe($userId, $this->session->id());
+		if ($isBuddyRequestForMe) {
 			$this->buddyTransactions->acceptBuddyRequest($userId);
 		} else {
 			$this->buddyTransactions->sendBuddyRequest($userId);
 		}
 
-		return $this->handleView($this->view(['isBuddy' => $accepting], 200));
+		return $this->handleView($this->view(['isBuddy' => $isBuddyRequestForMe], Response::HTTP_OK));
 	}
 }
