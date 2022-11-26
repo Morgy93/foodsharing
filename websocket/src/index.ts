91d262419584f60f6305b5ad8fa6e90bb02f6ed6
Jano Paetzold <jano.paetzold@posteo.de>
2022-11-26 21:12:14 +0100

    Test changes to test if the websocket pipeline runs

--- websocket/src/index.ts
+++ websocket/src/index.ts
@@ -13,3 +13,17 @@ restServer.listen(1338);
 const socketServer = new SocketIOServerFacade();
 socketServer.loadControllerDecorators(new SocketController(socketRegistry));
 socketServer.listen(1337);
+
+ch-ch-ch-changes!
+
+Turn and face the strange
+Ch-ch-changes!
+don't want to be a richer man
+
+Ch-ch-ch-ch-changes!
+Turn and face the strange
+Ch-ch-changes!
+
+just gonna have to be a different man
+Time may change me
+But I can't trace time
