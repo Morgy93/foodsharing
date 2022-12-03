005ab60a13459b57275d8945b1f74448d558be34
martincodes-de <6265987-martincodes-de@users.noreply.gitlab.com>
2022-12-03 11:13:42 +0100

    add pseudo code to define how the command work

--- src/Command/MergeChatConversationsCommand.php
+++ src/Command/MergeChatConversationsCommand.php
@@ -8,6 +8,37 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * How does the command work?
+ * --------------------------
+ * 1. Fetch all already-duplicate-checked messages
+ * 2. Fetch an amount of messages after a defined date [yyyy-mm-dd] and excluded already-duplicate-checked messages
+ * 3. For each message, ...
+ * 		1. check if the conversation is between two foodsaver [2 rows with conversation_id existing]
+ * 		if not --> save message in already-duplicate-checked messages and TERMINATE ITERATION
+ *
+ * 		2. check if the two foodsaver have more than 1 conversation between them
+ * 		if not --> save message in already-duplicate-checked messages and TERMINATE ITERATION
+ *
+ * 		3. For each conversation, ...
+ * 			1. fetch the amount of messages and save it in a list
+ * 			2. sort the list and get the conversation with the most messages
+ * 		return conversations and the conversation with the most messages
+ *
+ * 		4. For each conversation that is not the conversation with the most messages, ...
+ * 			1. move all messages in the conversation to the conversation with the most messages
+ * 			2. delete conversation
+ * 		the conversation with the most messages is now the merged conversation
+ *
+ * 		5. Fetch all messages of the merged conversation [fs_msg]
+ * 		6. get the last message of the merged conversation [fs_msg]
+ * 		7. Update the last message information for the merged conversation [fs_conversation]
+ * 		8. add all messages of the merged conversation to the already-duplicate-checked messages
+ *
+ * 		TERMINATE ITERATION
+ * 4. save all already-duplicate-checked messages
+ */
+
 #[AsCommand(
 	name: 'foodsharing:merge-chat-conversations',
 	description: '',
