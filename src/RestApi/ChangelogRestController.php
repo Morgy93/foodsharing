fe9c0b1a737b275f26eccd25276aad72f510e72d
martincodes-de <6265987-martincodes-de@users.noreply.gitlab.com>
2022-12-20 12:27:26 +0100

    add changelog configuration for categories

--- src/RestApi/ChangelogRestController.php
+++ src/RestApi/ChangelogRestController.php
@@ -4,28 +4,42 @@ namespace Foodsharing\RestApi;
 
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations\Get;
+use Http\Client\Exception\HttpException;
 use Martincodes\YamlChangelogGenerator\YamlChangelogCreator;
 use OpenApi\Annotations as OA;
 use OpenApi\Attributes\Tag;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException;
+use Symfony\Component\Yaml\Yaml;
 
-class ChangelogRestController extends AbstractFOSRestController
+final class ChangelogRestController extends AbstractFOSRestController
 {
 	/**
 	 * Returns the changelog since 2023
 	 * @OA\Response(response="200", description="Success")
+	 * @OA\Response(response="500", description="Error")
 	 */
 	#[Get('changelog/')]
 	#[Tag('system')]
 	public function getChangelogAction(): Response
 	{
-		$changelogGenerator = new YamlChangelogCreator(
-			__DIR__ . '/../../changelog/',
-			['release.yaml', 'readme.md'],
-			'release.yaml',
-			'-fs-release-'
-		);
+		$pathOfChangelogDirectory = __DIR__ . '/../../changelog/';
 
-		return $this->handleView($this->view($changelogGenerator->getChangelog()));
+		try {
+			$changelogConfiguration = Yaml::parseFile($pathOfChangelogDirectory . 'config.yaml');
+
+			$changelogGenerator = new YamlChangelogCreator(
+				$pathOfChangelogDirectory,
+				['release.yaml', 'readme.md'],
+				'release.yaml',
+				'-fs-release-'
+			);
+			return $this->handleView($this->view([
+				'changelog' => $changelogGenerator->getChangelog(),
+				'meta' => $changelogConfiguration,
+			]));
+		} catch (\Exception $e) {
+			throw new ServiceUnavailableHttpException();
+		}
 	}
 }
