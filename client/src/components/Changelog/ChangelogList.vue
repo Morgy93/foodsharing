fe668427c462daafa54885cf228e7045f8d06c95
martincodes-de <6265987-martincodes-de@users.noreply.gitlab.com>
2022-12-21 01:55:10 +0100

    add new changelogs and first version of vue component for changelog

--- client/src/components/Changelog/ChangelogList.vue
+++ client/src/components/Changelog/ChangelogList.vue
@@ -1,18 +1,97 @@
 <template>
-  <p>Here you can see our changes since 2023.</p>
+  <section>
+    <p>Here you can see our changes since 2023.</p>
+    <BTable
+      striped
+      hover
+      responsive
+      small
+      bordered
+      :items="changelogEntries"
+      :fields="headlines"
+    >
+      <template #cell(types_of_change)="data">
+        <ul>
+          <li
+            v-for="change in data.item.types_of_change"
+            :key="change"
+          >
+            {{ change }}
+          </li>
+        </ul>
+      </template>
+
+      <template #cell(authors)="data">
+        <span
+          v-for="author in data.item.authors"
+          :key="author"
+        >
+          <a
+            :href="getGitLabLinkForUser(author)"
+            target="_blank"
+          >{{ author }}</a>&nbsp;
+        </span>
+      </template>
+    </BTable>
+  </section>
 </template>
 
 <script>
 import { getChangelog } from '@/api/changelog'
+import { BTable } from 'bootstrap-vue'
 
 export default {
   name: 'ChangelogList',
   components: {
-
+    BTable,
+  },
+  data () {
+    return {
+      headlines: [
+        {
+          key: 'types_of_change',
+          label: 'Type of change',
+        },
+        {
+          key: 'description',
+        },
+        {
+          key: 'authors',
+          label: 'Contributors',
+        },
+        {
+          key: 'release',
+        },
+        {
+          key: 'added_at',
+          sortable: true,
+        },
+      ],
+      changelogEntries: [],
+    }
   },
   async mounted () {
-    this.changelog = await getChangelog()
-    console.log('CLG', this.changelog)
+    const changelog = await getChangelog()
+    this.updateDisplayedChangelogEntries(changelog.changelog)
+  },
+  methods: {
+    updateDisplayedChangelogEntries (changelog) {
+      const releases = Object.values(changelog)
+
+      releases.forEach(release => {
+        const releaseInformation = release.release
+
+        const changes = Object.values(release.changes)
+        changes.forEach(change => {
+          change.release = releaseInformation.name
+          change.added_at = this.$dateFormatter.date(change.added_at)
+          this.changelogEntries.push(change)
+        })
+      })
+    },
+    getGitLabLinkForUser (username) {
+      return `https://github.com/${username}/`
+    },
   },
 }
 </script>
