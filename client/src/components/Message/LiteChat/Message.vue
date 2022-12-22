e0568760fcab4c1034ade6c906f4ef322671fce1
martincodes-de <6265987-martincodes-de@users.noreply.gitlab.com>
2022-12-22 20:39:04 +0100

    add message component

---
+++ client/src/components/Message/LiteChat/Message.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <span><b>{{ authorName }}:</b></span>
+    <div>{{ message }}</div>
+    <div>
+      <small class="text-muted">{{ sentAt.toLocaleString() }}</small>
+    </div>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Message',
+  props: {
+    authorName: { type: String, required: true },
+    authorId: { type: Number, required: true },
+    message: { type: String, required: true },
+    sentAt: { type: Date, required: true },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
