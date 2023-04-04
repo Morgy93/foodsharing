7aaacea87bd5b9f48c07bf1563c010f7baca87e6
Christian Walgenbach <foodsharing@walgeo.de>
2023-04-04 15:18:56 +0200

    added forms

--- src/Modules/Basket/components/CreateOrEditBasket.vue
+++ src/Modules/Basket/components/CreateOrEditBasket.vue
@@ -8,6 +8,42 @@
     <div
       class="card-body p-0"
     />
+    <div class="row m-2">
+      <div class="col col-8">
+        <label>Beschreibung</label>
+        <b-form-textarea
+          id="forum-create-thread-form-body"
+          v-model="body"
+          rows="6"
+        />
+        <button
+          class="btn btn-primary"
+        >
+          {{ $i18n('upload.image') }}
+        </button>
+        <b-form-group
+          v-slot="{ ariaDescribedby }"
+          label="Individual radios"
+        >
+          <b-form-radio
+            v-model="selected"
+            :aria-describedby="ariaDescribedby"
+            name="some-radios"
+            value="A"
+          >
+            Option A
+          </b-form-radio>
+          <b-form-radio
+            v-model="selected"
+            :aria-describedby="ariaDescribedby"
+            name="some-radios"
+            value="B"
+          >
+            Option B
+          </b-form-radio>
+        </b-form-group>
+      </div>
+    </div>
   </div>
 </template>
 
