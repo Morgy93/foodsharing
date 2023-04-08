51df1b8799d75ee5d5eecabee55fa1d597cec525
martincodes <martin.schneider@techniknews.net>
2023-04-08 22:56:07 +0200

    change url

--- client/src/mixins/FeatureToggleMixin.js
+++ client/src/mixins/FeatureToggleMixin.js
@@ -1,7 +1,7 @@
 export default {
   methods: {
     async isFeatureFlagActive (featureFlagIdentifier) {
-      const url = `/api/isFeatureFlagActive/${featureFlagIdentifier}`
+      const url = `/api/featureflags/${featureFlagIdentifier}`
       const response = await fetch(url)
       const data = await response.json()
       return data.isActive === true
