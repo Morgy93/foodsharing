9082e07501da7dba2e199a54ac70b10de307c883
Christian Walgenbach <foodsharing@walgeo.de>
2023-06-08 21:14:07 +0200

    added MailboxManageEdit and MailboxManageOverview

--- src/Modules/Mailbox/components/MailboxManage.vue
+++ src/Modules/Mailbox/components/MailboxManage.vue
@@ -1,65 +1,24 @@
 <template>
   <div>
-    <b-row>
-      <b-col
-        cols="12"
-        md="10"
-      >
-        {{ $i18n('mailbox.manage') }}
-      </b-col>
-      <b-col
-        cols="12"
-        md="2"
-      >
-        <b-button
-          variant="primary"
-        >
-          Mailbox erstellen
-        </b-button>
-      </b-col>
-    </b-row>
-    <b-row
-      v-for="mailbox in mailboxes"
-      :key="mailbox.id"
-      class="pb-2 pt-2"
-    >
-      <b-col class="bg-white">
-        <h4 class="pb-2">
-          {{ mailbox.name }}
-        </h4>
-        <div>{{ mailbox.email_name }}</div>
-      </b-col>
-      <b-col
-        class="bg-white"
-      >
-        <h4
-          v-for="member in mailbox.member"
-          :key="member.id"
-        >
-          {{ mailbox.member.length }} Benutzer
-        </h4>
-        <span
-          v-for="member in mailbox.member"
-          :key="member.id"
-        >
-          {{ member.name }},
-        </span>
-      </b-col>
-    </b-row>
+    <MailboxManageOverview />
+    <MailboxManageEdit v-if="enableEditMode" />
   </div>
 </template>
 
 <script>
-import { getMemberBoxes } from '@/api/mailbox'
+import MailboxManageOverview from './MailboxManageOverview.vue'
+import MailboxManageEdit from './MailboxManageEdit.vue'
 
 export default {
+  components: {
+    MailboxManageOverview,
+    MailboxManageEdit,
+  },
   data () {
     return {
       mailboxes: [],
+      enableEditMode: false,
     }
-  },
-  async mounted () {
-    this.mailboxes = await getMemberBoxes()
   },
 }
 </script>
---
+++ src/Modules/Mailbox/components/MailboxManageOverview.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <b-row>
+      <b-col
+        cols="12"
+        md="10"
+      >
+        {{ $i18n('mailbox.manage') }}
+      </b-col>
+      <b-col
+        cols="12"
+        md="2"
+      >
+        <b-button
+          variant="primary"
+        >
+          Mailbox erstellen
+        </b-button>
+      </b-col>
+    </b-row>
+    <b-row
+      v-for="mailbox in mailboxes"
+      :key="mailbox.id"
+      class="pb-2 pt-2"
+    >
+      <b-col class="bg-white">
+        <h4 class="pb-2">
+          {{ mailbox.name }}
+        </h4>
+        <div>{{ mailbox.email_name }}</div>
+      </b-col>
+      <b-col
+        class="bg-white"
+      >
+        <b-row>
+          <b-col cols="10">
+            <h4
+              v-for="member in mailbox.member"
+              :key="member.id"
+            >
+              {{ mailbox.member.length }} Benutzer
+            </h4>
+            <span
+              v-for="member in mailbox.member"
+              :key="member.id"
+            >
+              {{ member.name }},
+            </span>
+          </b-col>
+          <b-col cols="2">
+            <b-button variant="outline-primary">
+              <i class="fas fa-fw fa-pencil-alt" />
+            </b-button>
+          </b-col>
+        </b-row>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import { getMemberBoxes } from '@/api/mailbox'
+
+export default {
+  data () {
+    return {
+      mailboxes: [],
+    }
+  },
+  async mounted () {
+    this.mailboxes = await getMemberBoxes()
+  },
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
