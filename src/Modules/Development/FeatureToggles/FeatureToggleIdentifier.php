eaa1606466fdeaf34e1525fb311bbd5350c79381
martincodes-de <6265987-martincodes-de@users.noreply.gitlab.com>
2023-06-15 21:00:57 +0200

    change method to get identifiers

--- src/Modules/Development/FeatureToggles/FeatureToggleIdentifier.php
+++ src/Modules/Development/FeatureToggles/FeatureToggleIdentifier.php
@@ -9,6 +9,7 @@ final class FeatureToggleIdentifier
     public const ALWAYS_TRUE_FOR_TESTING_PURPOSES = "alwaysTrueForTestingPurposes";
     public const ALWAYS_FALSE_FOR_TESTING_PURPOSES = "alwaysFalseForTestingPurposes";
 
+
     /**
      * Returns all feature toggle identifiers.
      * @return array<String, String>
@@ -16,6 +17,7 @@ final class FeatureToggleIdentifier
     public static function all(): array
     {
         $identifierDefinitions = new ReflectionClass(self::class);
-        return $identifierDefinitions->getConstants();
+        return array_values($identifierDefinitions->getConstants());
     }
+
 }
