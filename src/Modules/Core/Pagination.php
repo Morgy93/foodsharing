a60425081af932892fd19c849c4b8124385c7e00
KoeMai <koemai@outlook.com>
2023-06-22 20:00:21 +0000

    remove unused pagination methods

--- src/Modules/Core/Pagination.php
+++ src/Modules/Core/Pagination.php
@@ -12,30 +12,4 @@ class Pagination
      * @Assert\Positive()
      */
     public ?int $pageSize = null;
-
-    /**
-     * Offset to start.
-     *
-     * @Assert\Positive()
-     */
-    public int $offset = 0;
-
-    public function buildSqlLimit(): string
-    {
-        if ($this->pageSize || $this->pageSize != 0) {
-            return ' LIMIT :page_size OFFSET :start_item_index ';
-        }
-
-        return '';
-    }
-
-    public function addSqlLimitParameters(array $params): array
-    {
-        if ($this->pageSize || $this->pageSize != 0) {
-            $params['start_item_index'] = $this->offset;
-            $params['page_size'] = $this->pageSize;
-        }
-
-        return $params;
-    }
 }
