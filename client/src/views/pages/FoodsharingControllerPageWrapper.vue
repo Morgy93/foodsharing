994ddd653333a209ddfa5fe8f6d5951e66e8c7cc
Martin <martin.schneider@techniknews.net>
2023-06-26 17:11:06 +0000

    add notice

--- client/src/views/pages/FoodsharingControllerPageWrapper.vue
+++ client/src/views/pages/FoodsharingControllerPageWrapper.vue
@@ -1,3 +1,7 @@
+// This component should be used if you want to display a new page with a controller that extends FoodsharingController
+// it adds the margin to your website, thats it
+//
+
 <template>
   <div class="my-5">
     <slot />
