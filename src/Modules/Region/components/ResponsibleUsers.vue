2ca4bf85260ea7d954761cfe9279aa885607f8a2
Christian Walgenbach <foodsharing@walgeo.de>
2023-08-06 19:25:42 +0200

    added responsible users

---
+++ src/Modules/Region/components/ResponsibleUsers.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Container :title="title">
+      <div
+        v-for="responsibleUser in responsibleUsers"
+        :key="responsibleUser.id"
+      >
+        <Avatar :url="responsibleUser.imageUrl" />
+        {{ responsibleUser.user.name }}
+      </div>
+    </container>
+  </div>
+</template>
+
+<script>
+import Container from '@/components/Container/Container.vue'
+import Avatar from '@/components/Avatar.vue'
+
+export default {
+  components: { Avatar, Container },
+  props: {
+    title: { type: String, required: true },
+    responsibleUsers: { type: Array, required: true },
+  },
+}
+</script>
