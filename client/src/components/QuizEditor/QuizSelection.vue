85918654e1a3bc25f4cd90377183ffe96148051e
Anton Ballmaier <aballmaier@posteo.de>
2023-09-03 22:19:48 +0200

    Add vue Quiz Selection

---
+++ client/src/components/QuizEditor/QuizSelection.vue
@@ -0,0 +1,36 @@
+<template>
+  <Container
+    :title="'Quizze'"
+  >
+    <a
+      v-for="quiz in visibleQuizes"
+      :key="quiz.id"
+      type="button"
+      class="list-group-item list-group-item-action"
+      :href="`/quiz/edit/${quiz.id}`"
+      v-text="quiz.name"
+    />
+  </Container>
+</template>
+
+<script>
+import Container from '@/components/Container/Container.vue'
+
+export default {
+  components: {
+    Container,
+  },
+  props: {
+    visibleQuizes: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: { },
+}
+</script>
+<style lang="scss" scoped>
+.list-group-item:not(:last-child) {
+  border-bottom: 0;
+}
+</style>
