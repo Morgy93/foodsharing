ef611c38bafec7ed3b7f043742e26245d479b379
fridtjof <fridtjof@das-labor.org>
2023-09-15 18:56:47 +0200

    XhrResponses: make PERMISSION_DENIED an array so it works with array type signatures

--- src/Lib/Xhr/XhrResponses.php
+++ src/Lib/Xhr/XhrResponses.php
@@ -4,12 +4,7 @@ namespace Foodsharing\Lib\Xhr;
 
 class XhrResponses
 {
-    public const PERMISSION_DENIED = 'permission_denied';
-
-    public function fail_permissions()
-    {
-        return self::PERMISSION_DENIED;
-    }
+    public const PERMISSION_DENIED = ['permission_denied'];
 
     public function fail_generic()
     {
