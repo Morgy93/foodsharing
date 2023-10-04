0b8ef7ff8147c6fcfec425c2f08db957c30119c6
Anton Ballmaier <aballmaier@posteo.de>
2023-10-04 14:46:43 +0200

    add store result type

---
+++ client/src/components/SearchBar/ResultEntry/StoreResultEntry.vue
@@ -0,0 +1,70 @@
+<template>
+  <a
+    :href="$url('store', store.id)"
+    class="d-flex dropdown-item search-result"
+  >
+    <div class="text-truncate flex-grow-1">
+      <h6 class="m-0 text-truncate d-inline">
+        <i
+          v-if="store.is_manager"
+          v-b-tooltip.noninteractive="'Du bist Verantwortlich für diesen Betrieb'"
+          class="fas fa-user-cog"
+        />
+        <i
+          v-else-if="is_member"
+          v-b-tooltip.noninteractive="'Du bist Mitglied dieses Betriebs'"
+          class="fas fa-user-check"
+        />
+        <i
+          v-else-if="is_jumper"
+          v-b-tooltip.noninteractive="'Du bist Springer dieses Betriebs'"
+          class="fas fa-running"
+        />
+        {{ store.name }}
+      </h6>
+      <br>
+      <small class="separate">
+        <span v-if="store.region_id">
+          in
+          <a :href="$url('stores', store.region_id)">
+            {{ store.region_name }}
+          </a>
+        </span>
+        <span>
+          {{ $i18n(`storestatus.${store.cooperation_status}`) }}
+        </span>
+        <span v-if="store.city">
+          <span v-if="store.street">{{ store.street }},</span>
+          <span v-if="store.zip">{{ store.zip }}</span>
+          {{ store.city }}
+        </span>
+      </small>
+    </div>
+  </a>
+</template>
+<script>
+
+export default {
+  components: { },
+  props: {
+    store: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    is_member () {
+      return this.store.membership_status === 1
+    },
+    is_jumper () {
+      return this.store.membership_status === 2
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.separate>*:not(:last-child)::after {
+  content: '•';
+}
+</style>
