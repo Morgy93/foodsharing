6adada192881ea4c6afa769baa7af6243f40ebde
Anton Ballmaier <aballmaier@posteo.de>
2023-11-26 11:57:48 +0100

    add rest endpoint

--- src/RestApi/BuddyRestController.php
+++ src/RestApi/BuddyRestController.php
@@ -35,7 +35,7 @@ class BuddyRestController extends AbstractFOSRestController
      * @OA\Response(response="200", description="Success.", @OA\Schema(type="object",
      *     @OA\Property(property="isBuddy", type="integer", description="whether the other user is now this user's buddy")
      * ))
-     * @OA\Response(response="400", description="Already buddy with that user.")
+     * @OA\Response(response="400", description="Already send a request to that user.")
      * @OA\Response(response="403", description="Insufficient permissions to send the request.")
      * @OA\Tag(name="buddy")
      * @Rest\Put("buddy/{userId}", requirements={"userId" = "\d+"})
@@ -46,11 +46,11 @@ class BuddyRestController extends AbstractFOSRestController
             throw new UnauthorizedHttpException('');
         }
 
-        if (in_array($userId, $this->buddyGateway->listBuddyIds($this->session->id()))) {
-            throw new BadRequestHttpException();
+        if ($this->buddyGateway->buddyRequestedUser($this->session->id(), $userId)) {
+            throw new BadRequestHttpException('You cannot send mutliple requests');
         }
 
-        $accepting = $this->buddyGateway->buddyRequestedMe($userId, $this->session->id());
+        $accepting = $this->buddyGateway->buddyRequestedUser($userId, $this->session->id());
         if ($accepting) {
             $this->buddyTransactions->acceptBuddyRequest($userId);
         } else {
@@ -58,5 +58,30 @@ class BuddyRestController extends AbstractFOSRestController
         }
 
         return $this->handleView($this->view(['isBuddy' => $accepting], 200));
+    }
+
+    /**
+     * Removes a buddy request to a user.
+     *
+     * @OA\Parameter(name="userId", in="path", @OA\Schema(type="integer"), description="which user to remove the request to")
+     * @OA\Response(response="200", description="Success.", @OA\Schema(type="object"))
+     * @OA\Response(response="400", description="Not currently requested to be a buddy of that user.")
+     * @OA\Response(response="403", description="Insufficient permissions to send the request.")
+     * @OA\Tag(name="buddy")
+     * @Rest\Delete("buddy/{userId}", requirements={"userId" = "\d+"})
+     */
+    public function removeRequestAction(int $userId): Response
+    {
+        if (!$this->session->id()) {
+            throw new UnauthorizedHttpException('');
+        }
+
+        if (!$this->buddyGateway->buddyRequestedUser($this->session->id(), $userId)) {
+            throw new BadRequestHttpException('You cannot delete a request you did not send');
+        }
+
+        $this->buddyTransactions->removeBuddyRequest($userId);
+
+        return $this->handleView($this->view(true, 200));
     }
 }
