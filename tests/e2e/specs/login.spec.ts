c614924d3cfb78005a2720c68972ec6cde74c199
Thomas Hauschild <6517150-thomas.hauschild@users.noreply.gitlab.com>
2023-12-19 10:34:24 +0100

    [#1754] migrate LoginCest

---
+++ tests/e2e/specs/login.spec.ts
@@ -0,0 +1,46 @@
+import { expect, test } from "@playwright/test";
+
+test.describe("user", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+  });
+
+  type Cookie = {
+    name: string;
+    expires: number;
+  };
+
+  function getSessionCookie(cookies: Cookie[]) {
+    return cookies.find((cookie) => cookie.name === "PHPSESSID");
+  }
+
+  test("can login", async ({ page }) => {
+    await page.getByRole("button", { name: "Einloggen Einloggen" }).click();
+    await page.getByLabel("Einloggen", { exact: true }).click();
+    await expect(
+      page.getByRole("heading", { name: "Hallo Bot! Schön, dass du" }),
+    ).toBeVisible();
+
+    const sessionCookie = getSessionCookie(await page.context().cookies());
+    expect(sessionCookie.expires).toBe(-1);
+  });
+
+  test("can login permanent", async ({ page }) => {
+    await page.getByRole("button", { name: "Einloggen Einloggen" }).click();
+    await page.getByLabel("Dauerhaft eingeloggt bleiben").check();
+    await page.getByLabel("Einloggen", { exact: true }).click();
+    await expect(
+      page.getByRole("heading", { name: "Hallo Bot! Schön, dass du" }),
+    ).toBeVisible();
+
+    const sessionCookie = getSessionCookie(await page.context().cookies());
+    expect(sessionCookie.expires).toBeGreaterThan(0);
+
+    await page
+      .getByRole("button", { name: "Profilbild 👋 Hallo Bot!" })
+      .click();
+    await page.getByRole("menuitem", { name: " Abmelden" }).click();
+    await page.getByRole("button", { name: "Einloggen Einloggen" }).click();
+    await expect(page.getByLabel("Dauerhaft eingeloggt bleiben")).toBeChecked();
+  });
+});
