11bafc37f51cb3e11a01202306b54b0f4b8e741f
Anton Ballmaier <aballmaier@posteo.de>
2023-12-20 11:11:34 +0100

    rework annotations

--- src/RestApi/BuddyRestController.php
+++ src/RestApi/BuddyRestController.php
@@ -8,7 +8,7 @@ use Foodsharing\Modules\Buddy\BuddyGateway;
 use Foodsharing\Modules\Buddy\BuddyTransactions;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use OpenApi\Annotations as OA;
+use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -32,16 +32,14 @@ class BuddyRestController extends AbstractFOSRestController
 
     /**
      * Sends a buddy request to a user.
-     *
-     * @OA\Parameter(name="userId", in="path", @OA\Schema(type="integer"), description="which user to send the request to")
-     * @OA\Response(response="200", description="Success.", @OA\Schema(type="object",
-     *     @OA\Property(property="isBuddy", type="integer", description="whether the other user is now this user's buddy")
-     * ))
-     * @OA\Response(response="400", description="Already send a request to that user.")
-     * @OA\Response(response="403", description="Insufficient permissions to send the request.")
-     * @OA\Tag(name="buddy")
-     * @Rest\Put("buddy/{userId}", requirements={"userId" = "\d+"})
      */
+    #[OA\Tag(name: "buddy")]
+    #[OA\Parameter(name: "userId", in: "path", schema: new OA\Schema(type: "integer"), description: "which user to send the request to")]
+    #[OA\Response(response: "200", description: "Success.", content: new OA\JsonContent(type: "object", properties: [new OA\Property(property: "isBuddy", type: "integer", description: "whether the other user is now this user's buddy")]))]
+    #[OA\Response(response: "400", description: "Already send a request to that user.")]
+    #[OA\Response(response: "403", description: "Insufficient permissions to send the request.")]
+    #[Rest\Put("buddy/{userId}", requirements: ["userId" => "\d+"])]
+    
     public function sendRequestAction(int $userId): Response
     {
         if (!$this->session->id()) {
@@ -64,14 +62,13 @@ class BuddyRestController extends AbstractFOSRestController
 
     /**
      * Removes a buddy request to a user.
-     *
-     * @OA\Parameter(name="userId", in="path", @OA\Schema(type="integer"), description="which user to remove the request to")
-     * @OA\Response(response="200", description="Success.", @OA\Schema(type="object"))
-     * @OA\Response(response="400", description="Not currently requested to be a buddy of that user.")
-     * @OA\Response(response="403", description="Insufficient permissions to send the request.")
-     * @OA\Tag(name="buddy")
-     * @Rest\Delete("buddy/{userId}", requirements={"userId" = "\d+"})
      */
+    #[OA\Parameter(name: "userId", in: "path", schema: new OA\Schema(type: "integer"), description: "which user to remove the request to")]
+    #[OA\Response(response: "200", description: "Success.", content: new OA\JsonContent(type: "object"))]
+    #[OA\Response(response: "400", description: "Not currently requested to be a buddy of that user.")]
+    #[OA\Response(response: "403", description: "Insufficient permissions to send the request.")]
+    #[OA\Tag(name: "buddy")]
+    #[Rest\Delete("buddy/{userId}", requirements: ["userId" => "\d+"])]
     public function removeRequestAction(int $userId): Response
     {
         if (!$this->session->id()) {
